73cd316e96901f0a6435bad2c7c724b2a9ec812d
Tataro <tat.kittitat@gmail.com>
2024-09-02 00:18:25 +0700

    feat: add tailwind & style login page

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -10,5 +10,5 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() type: string = 'button';
   @Input() disabled: boolean = false;
-  @Input() class: string = '';
+  @Input() className: string = '';
 }
